a8d82d0cd570c2de5b919e6c1b86b2fb1bc24fc8
Stephen Kipkurui <stephen.kipkurui@yahoo.com>
2022-03-18 12:34:54 -0500

    Zillow Scaling

--- prepare.py
+++ prepare.py
@@ -1,61 +1,49 @@
-# Clean data function
-import acquire
-import os
+from wrangle import wrangle_zillow
+from operator import imod
 import env
 import pandas as pd
-
-
-iris = acquire.get_iris_data()
-
-def prep_iris(iris):
-
-    iris = iris.drop_duplicates()
-
-    iris = iris.drop(columns=['Unnamed: 0'])
-
-    iris_dummy = pd.get_dummies(iris[['sepal_width', 'petal_width']],
-                                drop_first=[True, True])
-
-    iris = pd.concat([iris, iris_dummy], axis=1)
-
-    return iris.drop(columns=['sepal_width', 'petal_width'])
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 
 
 
-
-row_titanic = acquire.get_titanic_data()
-
-def prep_titanic(row_titanic):
-    titanic_df = row_titanic.drop_duplicates()
-
-    titanic_df = titanic_df.drop(
-        columns=['deck', 'embarked', 'class', 'age', 'Unnamed: 0'])
-
-    titanic_df['embark_town'] = titanic_df.embark_town.fillna(
-        value='Southampton')
-
-    dummy_titanic = pd.get_dummies(
-        titanic_df[['sex', 'embark_town']], drop_first=[True, True])
-
-    titanic = pd.concat([dummy_titanic, titanic_df], axis=1)
-
-    return titanic.drop(columns=['sex', 'embark_town'])
-
-
-
-
-telco_data = acquire.get_telco_data()
-
-def prep_telco(telco_data):
+def min_max_scaller():
+   
+    min_max_scaler = sklearn.preprocessing.MinMaxScaler()
     
-    t_df = telco_data.drop_duplicates()
+    train, validate, test = wrangle_zillow()
+    min_max_scaler.fit(train)
+    x_train_min_max_scaller = min_max_scaler.transform(train)
+    x_validate_min_max_scaller = min_max_scaler.transform(validate)
+    x_test_min_max_scaller = min_max_scaler.transform(test)
     
-    t_df = t_df.drop(columns = ['Unnamed: 0', 'tech_support', 'streaming_tv', 'paperless_billing', 
-                   'streaming_movies', 'device_protection', 'online_backup', 'online_security'])
+    return x_train_min_max_scaller, x_validate_min_max_scaller, x_test_min_max_scaller
     
-    dummy_telco_data = pd.get_dummies(t_df[['churn','internet_service_type','payment_type']], 
-                                      drop_first = [True, True, True])
+
+def standard_scaller():
+
+    std_scaler = sklearn.preprocessing.StandardScaler()
+    train, validate, test = wrangle_zillow()
+    min_max_scaler.fit(train)
+    x_train_std_scaller = std_scaler.transform(train)
+    x_validate_std_scaller = std_scaler.transform(validate)
+    x_test_std_scaller = std_scaler.transform(test)
     
-    t_df = pd.concat([t_df, dummy_telco_data], axis = 1)
+    return x_train_std_scaller, x_validate_std_scaller, x_test_std_scaller
+
     
-    return t_df.drop(columns = ['churn','internet_service_type','payment_type'])
+def robust_scaller():
+    
+    robust_scaler = sklearn.preprocessing.RobustScaler()
+    train, validate, test = wrangle_zillow()
+    robust_scaler.fit(train)
+    x_train_std_scaller = robust_scaler.transform(train)
+    x_validate_std_scaller = robust_scaler.transform(validate)
+    x_test_std_scaller = robust_scaler.transform(test)
+    
+    return x_train_std_scaller, x_validate_std_scaller, x_test_std_scaller
+
+    
+    
